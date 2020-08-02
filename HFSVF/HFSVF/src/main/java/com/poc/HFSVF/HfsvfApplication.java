036a8373ff3a0f1c5877121d643e5a89ae10a6bc
danielteodosio <dferreirateodosio@gmail.com>
2020-08-01 22:48:24 -0300

    Primeiro commit do HFSVF.

---
+++ HFSVF/HFSVF/src/main/java/com/poc/HFSVF/HfsvfApplication.java
@@ -0,0 +1,13 @@
+package com.poc.HFSVF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfsvfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfsvfApplication.class, args);
+	}
+
+}
